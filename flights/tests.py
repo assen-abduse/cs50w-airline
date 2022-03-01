a079b7ecb2c85217c074f618692fefbf0aa273d8
assen-abduse <assen.abduse@gmail.com>
2022-03-01 21:49:50 +0300

    fixed tests issues

--- flights/tests.py
+++ flights/tests.py
@@ -57,19 +57,11 @@ class FlightsTestCase(TestCase):
 
     def test_valid_flight_page(self):
         a1 = Airport.objects.get(code="AAA")
-        a2 = Airport.objects.get(code="BBB")
-        f = Flight.objects.get(origin=a1, destination=a2)
+        f = Flight.objects.get(origin=a1, destination=a1)
 
         c = Client()
-        response = c.get(f"/flghts/{f.id}")
-        self.assertEqual(response.status_code, 404)
-
-    def test_invalid_flight_page(self):
-        max_id = Flight.objects.all().aggregate(Max("id"))["id__max"]
-
-        c = Client()
-        response = c.get(f"/flights/{max_id + 1}")
-        self.assertEqual(response.status_code, 404)
+        response = c.get(f"/flights/{f.id}")
+        self.assertEqual(response.status_code, 200)
 
     def test_flight_page_passengers(self):
         f = Flight.objects.get(pk=1)
